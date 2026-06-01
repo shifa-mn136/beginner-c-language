b02f60a7fe0aa33e3a0d6b8464f451224a8dea37
shifa-mn136 <shifa678mansuri@gmail.com>
2026-06-01 23:41:03 +0530

    Add files via upload

---
+++ invertedpyramid.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	for(a=4;a>=1;a--){
+		for(b=0;b<=4-a;b++){
+			printf(" ");
+		}
+		for(b=1;b<=(2*a-1);b++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
